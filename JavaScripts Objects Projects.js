c49824842a0359495c2e2d9e24bd53ed2c005ff4
Alves Gonzalez <alvesdgonzalez3@gmail.com>
2025-10-07 17:03:31 -0400

    Add files via upload

---
+++ JavaScripts Objects Projects.js
@@ -0,0 +1,52 @@
+let autoTodo = new function() {
+    this.clickItem = function(item) {
+        item.click();
+    };
+
+    this.toggleAll = function() {
+        document.querySelector('.toggle-all').click();
+    };
+
+    this.selectItemX = function(x) {
+        document.querySelector('ul.todo-list > li:nth-child(' + x + ') input.toggle').click();
+    };
+
+    this.deleteItemX = function(x) {
+        document.querySelector('ul.todo-list > li:nth-child(' + x + ') button.destroy').click();
+    };
+
+    this.clearCompleted = function() {
+        document.querySelector('button.clear-completed').click();
+    };
+
+    this.filterAll = function() {
+        location.hash = '/';
+    };
+
+    this.filterCompleted = function() {
+        location.hash = '/completed';
+    };
+
+    this.filterActive = function() {
+        location.hash = '/active';
+    };
+
+    this.createTodo = function() {
+        document.querySelector('body > section > header > input').value = 'name';
+        document
+            .querySelector('body > section > header > input')
+            .dispatchEvent(new Event('change', { bubbles: true }));
+    };
+
+    this.amendTodo = function(x) {
+        document
+            .querySelector('ul.todo-list > li:nth-child(' + x + ') label')
+            .dispatchEvent(new Event('dblclick', { bubbles: true }));
+        document
+            .querySelector('ul.todo-list > li:nth-child(' + x + ') .edit')
+            .value = 'amended';
+        document
+            .querySelector('ul.todo-list > li:nth-child(' + x + ') .edit')
+            .dispatchEvent(new Event('blur'));
+    };
+};
