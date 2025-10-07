284de1f6762cf20d98b4054b7cb00048ef4f4696
Alves Gonzalez <alvesdgonzalez3@gmail.com>
2025-10-07 18:14:04 -0400

    JavaScripts Objects Projects.js

--- JavaScripts Objects Projects.js
+++ JavaScripts Objects Projects.js
@@ -1,52 +1,59 @@
 let autoTodo = new function() {
     this.clickItem = function(item) {
         item.click();
-    };
+    }
+    ;
 
     this.toggleAll = function() {
         document.querySelector('.toggle-all').click();
-    };
+    }
+    ;
 
     this.selectItemX = function(x) {
         document.querySelector('ul.todo-list > li:nth-child(' + x + ') input.toggle').click();
-    };
+    }
+    ;
 
     this.deleteItemX = function(x) {
         document.querySelector('ul.todo-list > li:nth-child(' + x + ') button.destroy').click();
-    };
+    }
+    ;
 
     this.clearCompleted = function() {
         document.querySelector('button.clear-completed').click();
-    };
+    }
+    ;
 
     this.filterAll = function() {
         location.hash = '/';
-    };
+    }
+    ;
 
     this.filterCompleted = function() {
         location.hash = '/completed';
-    };
+    }
+    ;
 
     this.filterActive = function() {
         location.hash = '/active';
-    };
+    }
+    ;
 
-    this.createTodo = function() {
-        document.querySelector('body > section > header > input').value = 'name';
-        document
-            .querySelector('body > section > header > input')
-            .dispatchEvent(new Event('change', { bubbles: true }));
-    };
+    this.createTodo = function createTodo(name) {
+        document.querySelector('.new-todo').value = name;
+        document.querySelector('.new-todo').dispatchEvent(new Event('change',{
+            'bubbles': true
+        }));
+    }
+    ;
 
-    this.amendTodo = function(x) {
-        document
-            .querySelector('ul.todo-list > li:nth-child(' + x + ') label')
-            .dispatchEvent(new Event('dblclick', { bubbles: true }));
-        document
-            .querySelector('ul.todo-list > li:nth-child(' + x + ') .edit')
-            .value = 'amended';
-        document
-            .querySelector('ul.todo-list > li:nth-child(' + x + ') .edit')
-            .dispatchEvent(new Event('blur'));
-    };
-};
+    this.amendTodo = function(x, amendedValue) {
+        document.querySelector('ul.todo-list > li:nth-child(' + x + ') label').dispatchEvent(new Event('dblclick',{
+            bubbles: true
+        }));
+        document.querySelector('ul.todo-list > li:nth-child(' + x + ') .edit').value = amendedValue;
+        document.querySelector('ul.todo-list > li:nth-child(' + x + ') .edit').dispatchEvent(new Event('blur'));
+    }
+    ;
+}
+;
